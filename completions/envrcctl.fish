487883e367f7a03ef3b13f99e5c422d60dac4f9f
Rio Fujita <rio_github@rio.st>
2026-07-09 18:05:24 +0900

    house keeping

--- completions/envrcctl.fish
+++ completions/envrcctl.fish
@@ -2,19 +2,14 @@ function _envrcctl_completion;
     set -l response (env _ENVRCCTL_COMPLETE=fish_complete COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) envrcctl);
 
     for completion in $response;
-        set -l metadata (string split 
- $completion);
+        set -l metadata (string split "," $completion);
 
         if test $metadata[1] = "dir";
             __fish_complete_directories $metadata[2];
         else if test $metadata[1] = "file";
             __fish_complete_path $metadata[2];
         else if test $metadata[1] = "plain";
-            if test $metadata[3] != "_";
-                echo $metadata[2]	$metadata[3];
-            else;
-                echo $metadata[2];
-            end;
+            echo $metadata[2];
         end;
     end;
 end;
